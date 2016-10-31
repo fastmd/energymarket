4bd1e45c7bf10c03d481f15ff3881e8de0edd6cf
fastmd <sergelus@yandex.ru>
2016-10-31 16:54:58 +0200

    v 0.06

--- app/controllers/lineprs_controller.rb
+++ app/controllers/lineprs_controller.rb
@@ -1,13 +1,27 @@
 class LineprsController < ApplicationController
+  def create
+    @mp = Mpoint.find(params[:mpoint_id])
+    #@mv = @mp.mvalues.create(params[:mvalue])
+    @mv = @mp.mvalues.new
+    @mv.val = params[:val]
+    @mv.comment = params[:comment]
+    @mv.save
+    params[:num] = @mp.id
+    redirect_to mpoint_path(@mp)
+  end
+  
   def new
+    @mp = Mpoint.find(params[:mpoint_id])
+    @mv = @mp.mvalues.create(mvalue_params)
+  #  redirect_to contract_path(@contract.id)
+  end
+
+  def edit
   end
 
   def show
   end
 
   def index
-  end
-
-  def edit
   end
 end
