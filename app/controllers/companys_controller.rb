607fff654b3e7706851639e5d2e207e12294fb99
lolo <jdanov1@yandex.ru>
2017-01-06 11:00:30 +0200

    v.054 larisa

--- app/controllers/companys_controller.rb
+++ app/controllers/companys_controller.rb
@@ -50,7 +50,8 @@ class CompanysController < ApplicationController
     @ddatee2 = @ddate.change(day: 1)
     i=0
     j=0
-    suma = 0.0
+    sumaapr = 0.0
+    sumaaliv = 0.0
     losses = 0.0    
     @mp.each do |item|
       j = j+1
@@ -61,45 +62,58 @@ class CompanysController < ApplicationController
       if !@met.nil? then
         report_rind[3] = @met.meternum
         if @met.koefcalc.nil? then report_rind[7] = 1.to_i else report_rind[7] = @met.koefcalc.to_i end         
-        @mv1 = Mvalue.where("meter_id = ? AND (actp180 IS NOT NULL) AND (/*actdate >= ? AND*/ actdate <= ?)", @met.id, @ddateb1, @ddateb2)
-        @mv0 = Mvalue.where("meter_id = ? AND (actp180 IS NOT NULL) AND (/*actdate >= ? AND*/ actdate <= ?)", @met.id, @ddatee1, @ddatee2)
-        if @mv0.count !=0 then report_rind[4] = @mv0.order(:actdate, :created_at, :updated_at).last.actp180 else report_rind[4] = nil end       
-        if @mv1.count !=0 then report_rind[5] = @mv1.order(:actdate, :created_at, :updated_at).last.actp180 else report_rind[5] = nil end
+        mv1 = Mvalue.where("meter_id = ? AND (actp180 IS NOT NULL) AND (/*actdate >= ? AND*/ actdate <= ?)", @met.id, @ddateb1, @ddateb2)
+        mv0 = Mvalue.where("meter_id = ? AND (actp180 IS NOT NULL) AND (/*actdate >= ? AND*/ actdate <= ?)", @met.id, @ddatee1, @ddatee2)
+        if mv0.count !=0 then report_rind[4] = mv0.order(:actdate, :created_at, :updated_at).last.actp180 else report_rind[4] = nil end       
+        if mv1.count !=0 then report_rind[5] = mv1.order(:actdate, :created_at, :updated_at).last.actp180 else report_rind[5] = nil end
         report_rind[6] = (report_rind[4].to_f - report_rind[5].to_f).round 4            
         report_rind[8] = (report_rind[7] * report_rind[6]).round 4
-        suma +=  report_rind[8] 
+        wapr = report_rind[8]
+        sumaapr +=  report_rind[8] 
         @report[i] =  [report_rind[0],report_rind[1],report_rind[2]+' a/pr',report_rind[3],report_rind[4],report_rind[5],report_rind[6],report_rind[7],report_rind[8]]      
         i+=1
-        @mv1 = Mvalue.where("meter_id = ? AND (actp280 IS NOT NULL) AND (/*actdate >= ? AND*/ actdate <= ?)", @met.id, @ddateb1, @ddateb2)
-        @mv0 = Mvalue.where("meter_id = ? AND (actp280 IS NOT NULL) AND (/*actdate >= ? AND*/ actdate <= ?)", @met.id, @ddatee1, @ddatee2)
-        if @mv0.count !=0 then report_rind[4] = @mv0.order(:actdate, :created_at, :updated_at).last.actp280 else report_rind[4] = nil end       
-        if @mv1.count !=0 then report_rind[5] = @mv1.order(:actdate, :created_at, :updated_at).last.actp280 else report_rind[5] = nil end
+        mv1 = Mvalue.where("meter_id = ? AND (actp280 IS NOT NULL) AND (/*actdate >= ? AND*/ actdate <= ?)", @met.id, @ddateb1, @ddateb2)
+        mv0 = Mvalue.where("meter_id = ? AND (actp280 IS NOT NULL) AND (/*actdate >= ? AND*/ actdate <= ?)", @met.id, @ddatee1, @ddatee2)
+        if mv0.count !=0 then report_rind[4] = mv0.order(:actdate, :created_at, :updated_at).last.actp280 else report_rind[4] = nil end       
+        if mv1.count !=0 then report_rind[5] = mv1.order(:actdate, :created_at, :updated_at).last.actp280 else report_rind[5] = nil end
         report_rind[6] = (report_rind[4].to_f - report_rind[5].to_f).round 4           
-        report_rind[8] = (report_rind[7] * report_rind[6]).round 4   
+        report_rind[8] = (report_rind[7] * report_rind[6]).round 4
+        waliv = report_rind[8]
+        sumaaliv +=  report_rind[8]    
         @report[i] =  [nil,nil,report_rind[2]+' a/liv',nil,report_rind[4],report_rind[5],report_rind[6],report_rind[7],report_rind[8]]      
         i+=1 
-        @mv1 = Mvalue.where("meter_id = ? AND (actp380 IS NOT NULL) AND (/*actdate >= ? AND*/ actdate <= ?)", @met.id, @ddateb1, @ddateb2)
-        @mv0 = Mvalue.where("meter_id = ? AND (actp380 IS NOT NULL) AND (/*actdate >= ? AND*/ actdate <= ?)", @met.id, @ddatee1, @ddatee2)
-        if @mv0.count !=0 then report_rind[4] = @mv0.order(:actdate, :created_at, :updated_at).last.actp380 else report_rind[4] = nil end       
-        if @mv1.count !=0 then report_rind[5] = @mv1.order(:actdate, :created_at, :updated_at).last.actp380 else report_rind[5] = nil end
+        mv1 = Mvalue.where("meter_id = ? AND (actp380 IS NOT NULL) AND (/*actdate >= ? AND*/ actdate <= ?)", @met.id, @ddateb1, @ddateb2)
+        mv0 = Mvalue.where("meter_id = ? AND (actp380 IS NOT NULL) AND (/*actdate >= ? AND*/ actdate <= ?)", @met.id, @ddatee1, @ddatee2)
+        if mv0.count !=0 then report_rind[4] = mv0.order(:actdate, :created_at, :updated_at).last.actp380 else report_rind[4] = nil end       
+        if mv1.count !=0 then report_rind[5] = mv1.order(:actdate, :created_at, :updated_at).last.actp380 else report_rind[5] = nil end
         report_rind[6] = (report_rind[4].to_f - report_rind[5].to_f).round 4            
-        report_rind[8] = (report_rind[7] * report_rind[6]).round 4   
+        report_rind[8] = (report_rind[7] * report_rind[6]).round 4 
+        wrpr = report_rind[8]  
         @report[i] =  [nil,nil,report_rind[2]+' r/pr',nil,report_rind[4],report_rind[5],report_rind[6],report_rind[7],report_rind[8]]      
         i+=1
-        @mv1 = Mvalue.where("meter_id = ? AND (actp480 IS NOT NULL) AND (/*actdate >= ? AND*/ actdate <= ?)", @met.id, @ddateb1, @ddateb2)
-        @mv0 = Mvalue.where("meter_id = ? AND (actp480 IS NOT NULL) AND (/*actdate >= ? AND*/ actdate <= ?)", @met.id, @ddatee1, @ddatee2)
-        if @mv0.count !=0 then report_rind[4] = @mv0.order(:actdate, :created_at, :updated_at).last.actp480 else report_rind[4] = nil end       
-        if @mv1.count !=0 then report_rind[5] = @mv1.order(:actdate, :created_at, :updated_at).last.actp480 else report_rind[5] = nil end
+        mv1 = Mvalue.where("meter_id = ? AND (actp480 IS NOT NULL) AND (/*actdate >= ? AND*/ actdate <= ?)", @met.id, @ddateb1, @ddateb2)
+        mv0 = Mvalue.where("meter_id = ? AND (actp480 IS NOT NULL) AND (/*actdate >= ? AND*/ actdate <= ?)", @met.id, @ddatee1, @ddatee2)
+        if mv0.count !=0 then report_rind[4] = mv0.order(:actdate, :created_at, :updated_at).last.actp480 else report_rind[4] = nil end       
+        if mv1.count !=0 then report_rind[5] = mv1.order(:actdate, :created_at, :updated_at).last.actp480 else report_rind[5] = nil end
         report_rind[6] = (report_rind[4].to_f - report_rind[5].to_f).round 4            
-        report_rind[8] = (report_rind[7] * report_rind[6]).round 4  
+        report_rind[8] = (report_rind[7] * report_rind[6]).round 4 
+        wrliv = report_rind[8] 
         @report[i] =  [nil,nil,report_rind[2]+' r/liv',nil,report_rind[4],report_rind[5],report_rind[6],report_rind[7],report_rind[8]]      
+        i+=1 
+        cosfi = ((wapr ** 2 / (wapr ** 2 + wrpr ** 2))  ** 0.5).round 4 
+        @report[i] =  [nil,'cos fi /pr',nil,nil,nil,nil,nil,nil,cosfi]       
         i+=1  
+        cosfi = ((waliv ** 2 / (waliv ** 2 + wrliv ** 2))  ** 0.5).round 4          
+        @report[i] =  [nil,'cos fi /liv',nil,nil,nil,nil,nil,nil,cosfi]       
+        i+=1           
       end  
     end
         @report[i] =  [nil,'Потери в линии',nil,nil,nil,nil,nil,nil,losses]      
         i+=1 
-        @report[i] =  [nil,'Сумма',nil,nil,nil,nil,nil,nil,suma]       
-        i+=1         
+        @report[i] =  [nil,'Сумма a/pr',nil,nil,nil,nil,nil,nil,sumaapr]       
+        i+=1
+        @report[i] =  [nil,'Сумма a/liv',nil,nil,nil,nil,nil,nil,sumaaliv]       
+        i+=1                  
   end  
   
   def index
