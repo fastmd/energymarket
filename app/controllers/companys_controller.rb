4d515ce81792d3361a3410e084dcbcd0fb4c6405
lolo <jdanov1@yandex.ru>
2017-01-09 17:30:42 +0200

    v0.55 larisa

--- app/controllers/companys_controller.rb
+++ app/controllers/companys_controller.rb
@@ -5,13 +5,12 @@ class CompanysController < ApplicationController
   
   def create    
     @cp =  Company.new
-    @cp.furnizor_id = 1;
-    @cp.filial_id = 1;
-    @cp.name = params[:coname]
+    @cp.name = params[:name]
+    @cp.region = params[:region]
+    @cp.furnizor_id = params[:furnizor_id];
+    @cp.filial_id = params[:filial_id];    
     @cp.save
     redirect_to company_path(@cp)
-    #redirect_to @cp
-    #redirect_to mpoint_path(@cp)
   end
 
   def show
@@ -58,65 +57,66 @@ class CompanysController < ApplicationController
       report_rind[0] = j    
       report_rind[1] = "#{item.messtation} (#{item.clsstation})"
       report_rind[2] = "#{item.meconname} (#{item.clconname})"
+      report_rind[3] = "#{item.clconname}"
       @met = item.meters.first
       if !@met.nil? then
-        report_rind[3] = @met.meternum
-        if @met.koefcalc.nil? then report_rind[7] = 1.to_i else report_rind[7] = @met.koefcalc.to_i end         
+        report_rind[4] = @met.meternum
+        if @met.koefcalc.nil? then report_rind[8] = 1.to_i else report_rind[8] = @met.koefcalc.to_i end         
         mv1 = Mvalue.where("meter_id = ? AND (actp180 IS NOT NULL) AND (/*actdate >= ? AND*/ actdate <= ?)", @met.id, @ddateb1, @ddateb2)
         mv0 = Mvalue.where("meter_id = ? AND (actp180 IS NOT NULL) AND (/*actdate >= ? AND*/ actdate <= ?)", @met.id, @ddatee1, @ddatee2)
-        if mv0.count !=0 then report_rind[4] = mv0.order(:actdate, :created_at, :updated_at).last.actp180 else report_rind[4] = nil end       
-        if mv1.count !=0 then report_rind[5] = mv1.order(:actdate, :created_at, :updated_at).last.actp180 else report_rind[5] = nil end
-        report_rind[6] = (report_rind[4].to_f - report_rind[5].to_f).round 4            
-        report_rind[8] = (report_rind[7] * report_rind[6]).round 4
-        wapr = report_rind[8]
-        sumaapr +=  report_rind[8] 
-        @report[i] =  [report_rind[0],report_rind[1],report_rind[2]+' a/pr',report_rind[3],report_rind[4],report_rind[5],report_rind[6],report_rind[7],report_rind[8]]      
+        if mv0.count !=0 then report_rind[5] = mv0.order(:actdate, :created_at, :updated_at).last.actp180 else report_rind[5] = nil end       
+        if mv1.count !=0 then report_rind[6] = mv1.order(:actdate, :created_at, :updated_at).last.actp180 else report_rind[6] = nil end
+        report_rind[7] = (report_rind[5].to_f - report_rind[6].to_f).round 4            
+        report_rind[9] = (report_rind[8] * report_rind[7]).round 4
+        wapr = report_rind[9]
+        sumaapr +=  report_rind[9] 
+        @report[i] =  [report_rind[0],report_rind[1],report_rind[2]+' a/pr',report_rind[3],report_rind[4],report_rind[5],report_rind[6],report_rind[7],report_rind[8],report_rind[9]]      
         i+=1
         mv1 = Mvalue.where("meter_id = ? AND (actp280 IS NOT NULL) AND (/*actdate >= ? AND*/ actdate <= ?)", @met.id, @ddateb1, @ddateb2)
         mv0 = Mvalue.where("meter_id = ? AND (actp280 IS NOT NULL) AND (/*actdate >= ? AND*/ actdate <= ?)", @met.id, @ddatee1, @ddatee2)
-        if mv0.count !=0 then report_rind[4] = mv0.order(:actdate, :created_at, :updated_at).last.actp280 else report_rind[4] = nil end       
-        if mv1.count !=0 then report_rind[5] = mv1.order(:actdate, :created_at, :updated_at).last.actp280 else report_rind[5] = nil end
-        report_rind[6] = (report_rind[4].to_f - report_rind[5].to_f).round 4           
-        report_rind[8] = (report_rind[7] * report_rind[6]).round 4
-        waliv = report_rind[8]
-        sumaaliv +=  report_rind[8]    
-        @report[i] =  [nil,nil,report_rind[2]+' a/liv',nil,report_rind[4],report_rind[5],report_rind[6],report_rind[7],report_rind[8]]      
+        if mv0.count !=0 then report_rind[5] = mv0.order(:actdate, :created_at, :updated_at).last.actp280 else report_rind[5] = nil end       
+        if mv1.count !=0 then report_rind[6] = mv1.order(:actdate, :created_at, :updated_at).last.actp280 else report_rind[6] = nil end
+        report_rind[7] = (report_rind[5].to_f - report_rind[6].to_f).round 4           
+        report_rind[9] = (report_rind[8] * report_rind[7]).round 4
+        waliv = report_rind[9]
+        sumaaliv +=  report_rind[9]    
+        @report[i] =  [nil,nil,report_rind[2]+' a/liv',nil,nil,report_rind[5],report_rind[6],report_rind[7],report_rind[8],report_rind[9]]      
         i+=1 
         mv1 = Mvalue.where("meter_id = ? AND (actp380 IS NOT NULL) AND (/*actdate >= ? AND*/ actdate <= ?)", @met.id, @ddateb1, @ddateb2)
         mv0 = Mvalue.where("meter_id = ? AND (actp380 IS NOT NULL) AND (/*actdate >= ? AND*/ actdate <= ?)", @met.id, @ddatee1, @ddatee2)
-        if mv0.count !=0 then report_rind[4] = mv0.order(:actdate, :created_at, :updated_at).last.actp380 else report_rind[4] = nil end       
-        if mv1.count !=0 then report_rind[5] = mv1.order(:actdate, :created_at, :updated_at).last.actp380 else report_rind[5] = nil end
-        report_rind[6] = (report_rind[4].to_f - report_rind[5].to_f).round 4            
-        report_rind[8] = (report_rind[7] * report_rind[6]).round 4 
-        wrpr = report_rind[8]  
-        @report[i] =  [nil,nil,report_rind[2]+' r/pr',nil,report_rind[4],report_rind[5],report_rind[6],report_rind[7],report_rind[8]]      
+        if mv0.count !=0 then report_rind[5] = mv0.order(:actdate, :created_at, :updated_at).last.actp380 else report_rind[5] = nil end       
+        if mv1.count !=0 then report_rind[6] = mv1.order(:actdate, :created_at, :updated_at).last.actp380 else report_rind[6] = nil end
+        report_rind[7] = (report_rind[5].to_f - report_rind[6].to_f).round 4            
+        report_rind[9] = (report_rind[8] * report_rind[7]).round 4 
+        wrpr = report_rind[9]  
+        @report[i] =  [nil,nil,report_rind[2]+' r/pr',nil,nil,report_rind[5],report_rind[6],report_rind[7],report_rind[8],report_rind[9]]      
         i+=1
         mv1 = Mvalue.where("meter_id = ? AND (actp480 IS NOT NULL) AND (/*actdate >= ? AND*/ actdate <= ?)", @met.id, @ddateb1, @ddateb2)
         mv0 = Mvalue.where("meter_id = ? AND (actp480 IS NOT NULL) AND (/*actdate >= ? AND*/ actdate <= ?)", @met.id, @ddatee1, @ddatee2)
-        if mv0.count !=0 then report_rind[4] = mv0.order(:actdate, :created_at, :updated_at).last.actp480 else report_rind[4] = nil end       
-        if mv1.count !=0 then report_rind[5] = mv1.order(:actdate, :created_at, :updated_at).last.actp480 else report_rind[5] = nil end
-        report_rind[6] = (report_rind[4].to_f - report_rind[5].to_f).round 4            
-        report_rind[8] = (report_rind[7] * report_rind[6]).round 4 
-        wrliv = report_rind[8] 
-        @report[i] =  [nil,nil,report_rind[2]+' r/liv',nil,report_rind[4],report_rind[5],report_rind[6],report_rind[7],report_rind[8]]      
+        if mv0.count !=0 then report_rind[5] = mv0.order(:actdate, :created_at, :updated_at).last.actp480 else report_rind[5] = nil end       
+        if mv1.count !=0 then report_rind[6] = mv1.order(:actdate, :created_at, :updated_at).last.actp480 else report_rind[6] = nil end
+        report_rind[7] = (report_rind[5].to_f - report_rind[6].to_f).round 4            
+        report_rind[9] = (report_rind[8] * report_rind[7]).round 4 
+        wrliv = report_rind[9] 
+        @report[i] =  [nil,nil,report_rind[2]+' r/liv',nil,nil,report_rind[5],report_rind[6],report_rind[7],report_rind[8],report_rind[9]]      
         i+=1 
         cosfi = ((wapr ** 2 / (wapr ** 2 + wrpr ** 2))  ** 0.5).round 4 
-        @report[i] =  [nil,'cos fi /pr',nil,nil,nil,nil,nil,nil,cosfi]       
+        @report[i] =  [nil,'cos fi /pr',nil,nil,nil,nil,nil,nil,nil,cosfi]       
         i+=1  
         cosfi = ((waliv ** 2 / (waliv ** 2 + wrliv ** 2))  ** 0.5).round 4          
-        @report[i] =  [nil,'cos fi /liv',nil,nil,nil,nil,nil,nil,cosfi]       
+        @report[i] =  [nil,'cos fi /liv',nil,nil,nil,nil,nil,nil,nil,cosfi]       
         i+=1           
       end  
     end
-        @report[i] =  [nil,'Потери в линии',nil,nil,nil,nil,nil,nil,losses]      
+        @report[i] =  [nil,'Потери в линии',nil,nil,nil,nil,nil,nil,nil,losses]      
         i+=1 
-        @report[i] =  [nil,'Сумма a/pr',nil,nil,nil,nil,nil,nil,sumaapr]       
+        @report[i] =  [nil,'Сумма a/pr',nil,nil,nil,nil,nil,nil,nil,sumaapr]       
         i+=1
-        @report[i] =  [nil,'Сумма a/liv',nil,nil,nil,nil,nil,nil,sumaaliv]       
+        @report[i] =  [nil,'Сумма a/liv',nil,nil,nil,nil,nil,nil,nil,sumaaliv]       
         i+=1 
     respond_to do |format|
       format.html
-      format.pdf { send_data ListaReport.new.to_pdf(@cp,@luna,@report), :type => 'application/pdf', :filename => "lista.pdf" }
+      format.pdf { send_data ListaReport.new.to_pdf(@cp,@report,@luna,@ddate,@ddatee2,@ddateb2), :type => 'application/pdf', :filename => "lista.pdf" }
       format.xlsx { response.headers['Content-Disposition'] = 'attachment; filename="lista.xlsx"' }
     end                         
   end  
--- app/reports/lista_report.rb
+++ app/reports/lista_report.rb
@@ -1,19 +1,20 @@
 # encoding: utf-8
-class MpointsReport < Prawn::Document
+class ListaReport < Prawn::Document
   # ширина колонок
-  Widths = [20, 100, 70, 70, 70, 70, 120]
-  # заглавия колонок
-  Headers = ["№", "Дата актирования", "1.8.0", "2.8.0", "3.8.0", "4.8.0", "Комментарий"]
-  
-  def row(d1, d2, d3, d4, d5, d6, d7)
-    row = [d1, d2, d3, d4, d5, d6, d7]
+  Widths = [20, 100, 70, 50, 50, 50, 50, 50, 50, 50]
+      # заглавия колонок
+  Headers = ["№", "RRE,SE si liniilor", "Punctul de evidenta", "", "№ contor.", "Indicatii", 
+             "Indicatii", "Diferenta indicat.", "Coeficient contor.", "Energie kWh"]
+               
+  def row(d1, d2, d3, d4, d5, d6, d7, d8, d9, d10)
+    row = [d1, d2, d3, d4, d5, d6, d7, d8, d9, d10]
     make_table([row]) do |t|
       t.column_widths = Widths
       t.cells.style :borders => [:left, :right], :padding => 2
     end
   end
     
-  def to_pdf(mvalues,mpoints)
+  def to_pdf(cp,report,luna,ddate,ddatee2,ddateb2)
     # привязываем шрифты
     if RUBY_PLATFORM == "i386-mingw32" then
       font_families.update(
@@ -31,24 +32,21 @@ class MpointsReport < Prawn::Document
       font "OpenSans", :size => 10  
     end
     move_down(1)
-    text "История показаний", :size => 15, :style => :bold, :align => :center
-    move_down(18)
-    text "Наименование поставщика:  #{mpoints.company.furnizor.name}", :size => 12, :style => :bold, :align => :left     
-    text "Наименование потребителя: #{mpoints.company.name}", :size => 12, :style => :bold, :align => :left
-    text "Подстанция МЭ: #{mpoints.messtation}", :size => 12, :style => :bold, :align => :left 
-    text "Присоединение МЭ: #{mpoints.meconname}", :size => 12, :style => :bold, :align => :left 
-    text "Подстанция потребителя: #{mpoints.clsstation}", :size => 12, :style => :bold, :align => :left 
-    text "Присоединение потребителя: #{mpoints.clconname}", :size => 12, :style => :bold, :align => :left
-    move_down(28)  
-    # выборка записей
-    i=0      
+    text "LISTA", :size => 15, :style => :bold, :align => :center
+    text "de calcul a intrarii energiei electrice la #{cp.name}", :size => 14, :style => :bold, :align => :center
+    text "pentru luna #{luna} anul #{ddate.year}", :size => 15, :style => :bold, :align => :center
+    move_down(18) 
+    text "Denumirea furnizorului:  #{cp.furnizor.name}", :size => 12, :style => :bold, :align => :left 
+    move_down(10)  
+    # выборка записей 
     data = []
-    if !mvalues.nil? then
-    items = mvalues.each do |item|
-      data << row(i+=1, item.actdate.to_formatted_s(:day_month_year), item.actp180, item.actp280, item.actp380, item.actp480, item.comment)
+    if !report.nil? then
+      items = report.each do |item|
+        data << row(item[0], item[1], item[2], item[3], item[4], item[5], item[6], item[7], item[8], item[9])
+      end 
     end
-    end
-    
+    Headers[5] = "Indicatii \n #{ddatee2.to_formatted_s(:day_month_year)}"
+    Headers[6] = "Indicatii \n #{ddateb2.to_formatted_s(:day_month_year)}"
     head = make_table([Headers], :column_widths => Widths)
     table([[head], *(data.map{|d| [d]})], :header => true, :row_colors => %w[cccccc ffffff]) do
       row(0).style(:background_color => 'ffffff', :text_color => '000000', :font_style => :bold, :align => :center)
