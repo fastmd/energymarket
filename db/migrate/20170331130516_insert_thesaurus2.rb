97ddf0518c1f47b7126c029d3c0db25f114a46ad
lolo <jdanov1@yandex.ru>
2017-03-31 16:09:51 +0300

    v20170331 zmd-405ct

---
+++ db/migrate/20170331130516_insert_thesaurus2.rb
@@ -0,0 +1,8 @@
+class InsertThesaurus2 < ActiveRecord::Migration[5.0]
+  def up
+    execute("INSERT INTO public.thesaurus (name, cvalue, f, created_at, updated_at) VALUES ('meter', 'ZMD-405CT',true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);")
+  end
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end 
+end
