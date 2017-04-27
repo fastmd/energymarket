810059e6daf72405ff00e2c1d9fbb84145edade0
lolo <jdanov1@yandex.ru>
2017-04-27 16:20:44 +0300

    v2017042700 larisa filial and region on substation to mpoint

--- db/migrate/20170407084651_remake_views_vmpoints.rb
+++ db/migrate/20170407084651_remake_views_vmpoints.rb
@@ -110,7 +110,6 @@ class RemakeViewsVmpoints < ActiveRecord::Migration[5.0]
                  FROM vmpoints t1
                    JOIN vtrparams t2 ON t1.id = t2.mpoint_id
                 ORDER BY t1.company_id, t1.name, t1.id, t2.id;")  
-    execute("DROP VIEW public.vsstations;")
     execute("DELETE FROM public.thesaurus WHERE name = 'sstation';")  
   end  
   def down
