6eebbad70c08ac0c8099f00a317e4b08c95a0ab7
lolo <jdanov1@yandex.ru>
2017-06-21 11:03:56 +0300

    v2017062100 line-mpoints-number

--- app/models/lnparam.rb
+++ app/models/lnparam.rb
@@ -2,6 +2,7 @@ class Lnparam < ApplicationRecord
   belongs_to :mpoint
   validates_associated :mpoint
   belongs_to :line, -> { where("f=true").order('mesubstation_id','id') }, class_name: "Line", inverse_of: :lnparams, foreign_key: "line_id"
+  belongs_to :vallline, class_name: "Vallline", inverse_of: :lnparams, foreign_key: "line_id"
   validates_associated :line
   validates :line_id, presence: true, numericality: { only_integer: true } 
   validates :mpoint_id, presence: true, numericality: { only_integer: true }
--- app/models/vallline.rb
+++ app/models/vallline.rb
@@ -1,6 +1,7 @@
 class Vallline < ApplicationRecord
+  self.primary_key = 'id'
   belongs_to :wire, inverse_of: :valllines
   belongs_to :mesubstation, inverse_of: :valllines
-  has_many  :lnparams, inverse_of: :vallline
-  has_many  :mpoints, through: :lnparams, inverse_of: :vallline
+  has_many  :lnparams, inverse_of: :line, foreign_key: "line_id"
+  has_many  :mpoints, through: :lnparams, inverse_of: :line, foreign_key: "line_id"
 end
